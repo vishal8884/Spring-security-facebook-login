4629148910d20f93017982d5b5ab54e5322d6706
vishal <vishalambekar459@gmail.com>
2022-07-18 23:24:10 +0530

    code added

---
+++ Springboot-security-facebook-login/src/main/java/io/vishal/SpringbootSecurityFacebookLoginApplication.java
@@ -0,0 +1,15 @@
+package io.vishal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SpringbootSecurityFacebookLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityFacebookLoginApplication.class, args);
+	}
+
+}
